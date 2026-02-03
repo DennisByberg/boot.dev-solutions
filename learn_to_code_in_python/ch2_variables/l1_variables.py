30c055461e8e8f8f2f79f0c6e4472639b637ad45
dennisbyberg <dennis.byberg@hotmail.com>
2026-02-03 13:07:08 +0100

    add l1 & l2 solutions

---
+++ learn_to_code_in_python/ch2_variables/l1_variables.py
@@ -0,0 +1,8 @@
+# --- Assignment ---
+# We need to keep track of our hero's health!
+# On the first line of code, create a new variable named player_health and set it equal to 1000.
+
+# --- Answer ---
+player_health = 1000
+
+print(player_health)
