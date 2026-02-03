521be19fd6f173b160d7beeb418273843b8c65eb
dennisbyberg <dennis.byberg@hotmail.com>
2026-02-03 11:57:56 +0100

    add 1 more question

--- learn_to_code_in_python/introduction/what_is_python.py
+++ learn_to_code_in_python/introduction/what_is_python.py
@@ -1,2 +1,5 @@
-# Question: What's Python not really used for?
+# Question 1: What's Python not really used for?
 # Answer: Frontend Apps.
+
+# Question 2: Python is known for being...
+# Answer: Simple to read and write.
