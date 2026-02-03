67c5906cc582e7c717bece45c880552408f78cc2
dennisbyberg <dennis.byberg@hotmail.com>
2026-02-03 12:48:38 +0100

    add solutions

---
+++ learn_to_code_in_python/ch1_introduction/l10_syntax_errors_quiz.py
@@ -0,0 +1,5 @@
+# --- l9_syntax_errors_quiz ---
+# What happens if you try to run code with invalid syntax?
+
+# --- Answer ---
+# You'll get an error message from the Python interprater and the code won't execute
---
+++ learn_to_code_in_python/ch1_introduction/l9_syntax_errors_quiz.py
@@ -0,0 +1,5 @@
+# --- l9_syntax_errors_quiz ---
+# What does syntax mean?
+
+# --- Answer ---
+# The rules for valid code in a programming language
